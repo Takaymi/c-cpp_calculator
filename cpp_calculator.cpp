712214571f251bc016fb348cc445bac9f6371d3b
Takuyaa <takuyaa@superdev.com>
2026-04-18 13:24:15 -0700

    Added: Needed libs - Create numbers and operators function - Created user-inputs function

---
+++ cpp_calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+float FIRSTNUMBER, SECONDNUMBER;
+char OPERATOR;
+
+void USER_INPUTS(){
+
+    cout << "ENTER THE FIRST NUMBER -> ";
+    cin >> FIRSTNUMBER;
+    cout << "ENTER THE SECOND NUMBER -> ";
+    cin >> SECONDNUMBER;
+    cout << "ENTER THE OPERATOR -> ";
+    cin >> OPERATOR;
+}
